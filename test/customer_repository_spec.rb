bf8759b5a325feaf6e4e01348549b39f5352c8ad
Andre Pedro <andrerpedro170@gmail.com>
2022-06-05 17:00:49 -0600

    create feat and test for all customer instances

--- test/customer_repository_spec.rb
+++ test/customer_repository_spec.rb
@@ -9,6 +9,10 @@ RSpec.describe CustomerRepository do
     expect(@customer_repository).to be_a(CustomerRepository)
   end
 
+  it 'returns an array of all know customer instances' do
+    expect(@customer_repository.all.count).to eq(1000)
+  end
+
 
 
 end
